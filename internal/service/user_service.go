cd7753a4f3532a692ea893c6e2bed25627d7c404
Je2iK <74060484+Je2iK@users.noreply.github.com>
2026-09-15 18:50:42 +0700

    Fix formatting and spacing in UserService interface

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -6,15 +6,16 @@ import (
 	"practice/internal/repository"
 )
 
-type UserService interface{
-	Register(ctx context.Context, params models.CreateUserParams) (int, error)	
+type UserService interface {
+	Register(ctx context.Context, params models.CreateUserParams) (int, error)
 }
 type userService struct {
-	r *repository.UserRepository
+	r repository.UserRepository
 }
-func NewUserService(r *repository.UserRepository) UserService{
+
+func NewUserService(r repository.UserRepository) UserService {
 	return &userService{r: r}
 }
-func (s *userService) Register(ctx context.Context, params models.CreateUserParams) (int, error){
+func (s *userService) Register(ctx context.Context, params models.CreateUserParams) (int, error) {
 	return s.r.CreateUser(ctx, params.Name, params.Password)
-}	
+}
