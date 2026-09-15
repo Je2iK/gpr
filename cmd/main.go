605bae07aebdba2a456c6bbc03a6308075aeeaaf
Je2iK <74060484+Je2iK@users.noreply.github.com>
2026-09-15 18:48:36 +0700

    Clean up main function by removing unused code
    Removed unused imports and context handling from main function.

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,18 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"practice/internal/handlers"
-		"practice/internal/config"
-		"practice/internal/db"
-		"context"
+import (
+	"fmt"
+	"net/http"
+	"practice/internal/handlers"
 )
 
-func main(){
+func main() {
 	http.HandleFunc("/", handlers.HelloGo)
 	fmt.Println("Sta99900")
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("GetSuck")
 	}
-	ctx := context.Background()
-	cfg, err := config.Load()
-	if err != nil{
-		fmt.Println("gg")
-	}
-	pool, err:= db.NewPool(ctx, cfg.Database.URL)
-	if err != nil {
-		fmt.Println("gg")
-	}
-	defer pool.Close()
-	
+
 }
